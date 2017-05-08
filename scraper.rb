780d992ae8acd2f40a7f06d9cb5c39f0235972c4
Tony Bowden <tony@tmtm.com>
2017-05-08 15:29:33 +0100

    Also fetch via P39 positions

--- scraper.rb
+++ scraper.rb
@@ -7,5 +7,19 @@ names = WikiData::Category.new( "Category:Members of the Legislative Assembly of
         WikiData::Category.new( "Category:Speakers of the Legislative Assembly of Tonga", 'en').member_titles |
         WikiData::Category.new( "Category:Prime Ministers of Tonga", 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+query = <<EOS
+  SELECT DISTINCT ?item
+  WHERE
+  {
+    BIND(wd:Q21328621 AS ?membership)
+    BIND(wd:Q28861051 AS ?term)
+
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 ?membership .
+    ?position_statement pq:P2937 ?term .
+  }
+EOS
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { en: names })
 
